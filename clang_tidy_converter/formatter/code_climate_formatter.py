084fc768b09346a40fa6653c27c7ae37679c5fc5
Abhiroop Kodandapur Sanjeeva <abhiroopks@gmail.com>
2026-09-18 11:48:44 -0400

    stop md5 usage and use sha256

--- clang_tidy_converter/formatter/code_climate_formatter.py
+++ clang_tidy_converter/formatter/code_climate_formatter.py
@@ -129,7 +129,7 @@ class CodeClimateFormatter:
             return 'blocker'
 
     def _generate_fingerprint(self, message):
-        h = hashlib.md5()
+        h = hashlib.sha256()
         h.update(message.filepath.encode('utf8'))
         h.update(str(message.line).encode('utf8'))
         h.update(str(message.column).encode('utf8'))
